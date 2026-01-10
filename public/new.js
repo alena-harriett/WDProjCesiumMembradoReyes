b957e1b93667fc90a2dfc5cfec1c123ac8fcdec0
alena-harriett <b2029ahbmembrado@pshs.edu.ph>
2026-01-10 21:19:55 +0800

    gifss

--- public/new.js
+++
@@ -1,9 +0,0 @@
-const guestbookGallery = document.querySelector('.guestbook-gallery');
-if (guestbookGallery) {
-  guestbookGallery.addEventListener('wheel', function(e) {
-    if (Math.abs(e.deltaY) > Math.abs(e.deltaX)) {
-      e.preventDefault();
-      guestbookGallery.scrollLeft += e.deltaY;
-    }
-  }, { passive: false });
-}
